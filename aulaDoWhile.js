bff403972c02622be4a6cd07f4c511607c45d89d
Thiago Coelho <thiago.olivs.coelho@gmail.com>
2024-11-06 16:27:12 -0300

    ultimo commit do dia

---
+++ aulaDoWhile.js
@@ -0,0 +1,12 @@
+let usuario = {
+    nome: "Jhonny",
+    time: "Flamengo"
+}
+
+
+let mensagemDeBoasVindas = "Bem-vindo, torcedor do Fluminense";
+let mensagemDeBoasVindas1 = "Bem-vindo, torcedor do Flamengo";
+let mensagemDeBoasVindas2 = "Bem-vindo, torcedor do Vasco";
+let mensagemDeBoasVindas3 = "Bem-vindo, torcedor do Corinthians";
+let mensagemDeBoasVindas4 = "Bem-vindo, torcedor do Cruzeiro";
+let mensagemDeBoasVindasGeral = "Bem-vindo, amante do esporte!"
