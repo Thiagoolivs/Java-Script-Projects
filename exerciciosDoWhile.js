d0130b1b07ac0fc1c0bda43fa206b50990292ca4
Thiago <thiago.olivs.coelho@gmail.com>
2024-11-08 16:51:50 -0300

    Terminar ex. if else

--- exerciciosDoWhile.js
+++ exerciciosDoWhile.js
@@ -1,2 +1,21 @@
-let nome = "Thiago Coelho"
-let pietrogay = 12
+//exercícios Do While
+//ex1
+// const prompt = require (`prompt-sync`)();
+// let alunoCadastrado = [];
+// let continuar;
+
+// do {
+//     let nome = prompt(`Digite o nome do aluno: `);
+//     let idade = prompt(`Digite a idade do aluno: `)
+
+//     alunoCadastrado.push({nome: nome, idade: idade});
+
+//     continuar = prompt('Deseja continuar? (s/n) ');
+//     console.log('Aluno cadastrado com sucesso: ', alunoCadastrado);
+// } while(continuar.toLowerCase() === 's');
+
+//ex2
+const prompt = require (`prompt-sync`)();
+
+
+
